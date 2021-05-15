880352ee38d95734721099505ba8d2752c3d662d
54251579 <iotain2088@gmail.com>
2021-05-15 13:22:55 +0900

    track bar

--- py-opencv_Baisc/event_hadle/trackbar.py
+++ py-opencv_Baisc/event_hadle/trackbar.py
@@ -4,12 +4,13 @@ import numpy as np
 def value_change(value):
     canvas[:] = value
     cv.imshow('canvas', canvas)
+    print(cv.getTrackbarPos('value', 'canvas'))
 
 canvas = np.zeros((500,500), np.uint8)
 
 cv.namedWindow('canvas')
 cv.createTrackbar('value', 'canvas', 0, 255, value_change)
-
+cv.setTrackbarPos('value', 'canvas', 127)
 cv.imshow('canvas', canvas)
 cv.waitKey()
 cv.destroyAllWindows()
