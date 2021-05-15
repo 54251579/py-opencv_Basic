5500f7e72561f88a3585bb9526b137901c152974
54251579 <iotain2088@gmail.com>
2021-05-15 12:52:24 +0900

    key event

--- py-opencv_Baisc/event_hadle/keyboard_event.py
+++ py-opencv_Baisc/event_hadle/keyboard_event.py
@@ -1,9 +1,21 @@
 import cv2 as cv
 import sys
 
-path = './img/Lenna.png'
+path = # img path
 img = cv.imread(path)
 if img is None:
     print('img load failed')
     sys.exit()
 
+cv.namedWindow('img')
+cv.imshow('img', img)
+
+while True:
+    key = cv.waitKey()
+    if key == ord('I') or key == ord('i'):
+        img = ~img
+        cv.imshow('img', img)
+    elif key == ord('Q') or key == ord('q'):
+        break
+
+cv.destroyAllWindows()
